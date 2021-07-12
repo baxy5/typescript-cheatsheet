b558333c3e832deaba99c8f9e65ab588c231fcf7
baxy5 <bakos.soda@gmail.com>
2021-07-12 11:36:14 +0200

    typescript cheatsheet for me

---
+++ basic.ts
@@ -0,0 +1,21 @@
+console.log('Typescript!');
+
+// Typescript can tell you the origin of the error and some solution
+// Like:
+let order = 'first';
+
+// order.toLowercase() if you miss type it will give an error and the problem
+// with the lower 'c'.
+order.toLowerCase();
+
+// Also you can't assign a new value for order
+// Like this:
+// order = 1; -> it gives an error
+
+// Solution - do not assign value for your variable:
+let animal;
+
+// If you don't want to give a value for your variable but you know
+// what will it be then you can use:
+let computer : string;
+let orders : number;
