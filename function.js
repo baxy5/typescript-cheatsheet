6a0226df4bc3148267f7bc403c548820ab92b1b2
baxy5 <bakos.soda@gmail.com>
2021-07-21 09:53:59 +0200

    typescript function

---
+++ function.js
@@ -0,0 +1,6 @@
+function myNameFiveTimes(name, count) {
+    for (let i = 0; i < count; i++) {
+        console.log(`My name is ${name}`);
+    }
+}
+myNameFiveTimes("Bakos", 5);
---
+++ function.ts
@@ -0,0 +1,7 @@
+function myNameFiveTimes(name: string, count: number) {
+    for (let i = 0; i < count; i++) {
+        console.log(`My name is ${name}`);
+    }
+}
+
+myNameFiveTimes("Bakos", 5);
