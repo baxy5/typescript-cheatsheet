10ba0706543a5141c9036f06a4af85873ad21f14
baxy5 <bakos.soda@gmail.com>
2022-10-11 21:00:10 +0200

    basics

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+var addition = function (x, y) {
+    console.log(x + y);
+};
+addition(2, 3);
---
+++ sandbox.ts
@@ -0,0 +1,5 @@
+const addition = (x: number, y: number) => {
+    console.log(x + y);
+}
+
+addition(2, 3)
