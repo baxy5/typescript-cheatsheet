0b072fe6b3a4a08c68c0091a1e47792dd56d6c78
baxy5 <bakos.soda@gmail.com>
2022-10-11 21:13:08 +0200

    types

--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,41 @@
-const addition = (x: number, y: number) => {
+/* const addition = (x: number, y: number) => {
     console.log(x + y);
 }
 
-addition(2, 3)
+addition(2, 3) */
+
+// explicit types
+/* let character: string;
+let age: number;
+let isLoggedIn: boolean; */
+
+
+// arrays
+/* let ninjas: string[] = [];
+
+ninjas = [10,20]
+ninjas = ["asd", "asd"];
+ninjas.push("shaun")
+*/
+
+// union types
+/* let mixed: (string | number)[] = [];
+mixed.push("hello");
+mixed.push(20);
+mixed.push(false);
+
+let uid : string | number ;
+uid = "123"
+uid = 123 
+
+*/
+
+// objects
+/* let ninjaOne: object;
+ninjaOne = { name: "bakos", age: 20 }
+
+let ninjaTwo: {
+    name: string,
+    age: number,
+    beltColour: string
+} */
